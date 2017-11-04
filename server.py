da7fa1aba8027d22d6b6a06a256fbec7305344a0
34ndju <34ndju@gmail.com>
2017-11-03 23:42:15 -0500

    gitignore

--- server.py
+++ server.py
@@ -9,20 +9,25 @@ app = Flask(__name__)
 ask = Ask(app, "/one")
 
 def get_one():
-    return "one"
+    return "one response"
 
 @app.route("/")
 def homepage():
     return "hi there"
 
 @ask.launch
-def start_skil():
+def start_skill():
     welcome_message = "Welcome to my app hahahahaha. say... one"
     return question(welcome_message)
 
 @ask.intent("OneIntent")
 def return_one():
     number = get_one()
+    return statement(number)
+
+@ask.intent("TwoIntent")
+def return_one():
+    number = "two!!!"
     return statement(number)
 
 if __name__ == '__main__':
